de8aad79200a9eade02431822806dda1446dd55a
frankda <suyunyi@gmail.com>
2021-07-25 21:38:04 +1000

    - Finish Home screen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, FlatList, Button } from 'react-native';
+import { StyleSheet, TextInput, View, FlatList, Text } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../../App';
 import { listCharacters } from '../api/GetMarvel';
-import Icon from 'react-native-vector-icons/FontAwesome';
 import ListItem from '../components/ListItem';
 
 type ProfileScreenNavigationProp = StackNavigationProp<
@@ -11,35 +10,69 @@ type ProfileScreenNavigationProp = StackNavigationProp<
   'Home'
 >;
 interface IHero {
+    id: number,
     name: string,
     thumbnail: {
         path: string,
         extension: string
-    }
+    },
+    onPress: Function
 }
 
 const HomeScreen = ({ navigation }: { navigation: ProfileScreenNavigationProp }) => {
-    const [ heroes, setHeroes ] = useState([]);
+    const [ heroes, setHeroes ] = useState([] as IHero[]);
+    const [ filteredHeroes, setFilteredHeroes ] =useState([] as IHero[]);
+    const [ searchCopy, setSearchCopy ] = useState('');
 
     const fetchHeroesData = async () => {
         const res = await listCharacters();
         setHeroes(res.data.results);
-    } 
-
+    }
+    
     useEffect(() => {
         fetchHeroesData();
     }, []);
 
+    // update filtered results according to search text
+    // whenever heroes list or search text are updated
+    useEffect(() => {
+        handleSearch(searchCopy);
+    }, [heroes, searchCopy])
+    
+    const changeToProfileScreen = () => {
+        navigation.navigate('Profile', { name: 'hi' })
+    }
+
+    const handleSearch = (text: string) => {
+        setSearchCopy(text);
+        if (searchCopy) {
+            const filteredHeroes = heroes.filter((hero: IHero) => {
+                return hero.name.includes(searchCopy);
+            });
+            setFilteredHeroes(filteredHeroes);
+        } else {
+            setFilteredHeroes(heroes);
+        }
+    }
+
     return (
         <View>
-            <Button title="hi" onPress={() => navigation.navigate('Profile', { name: 'Jane' })} />
+            <TextInput
+                style={styles.textInput}
+                placeholder="Search for your hero!"
+                onChangeText={text => handleSearch(text)}
+                defaultValue={searchCopy}
+            />
+            <Text style={styles.resultsCount}>{filteredHeroes.length} results</Text>
             <FlatList 
-                data={heroes}
+                data={filteredHeroes}
+                keyExtractor={item => item.id.toString()}
                 renderItem={
                     ({item}: {item: IHero}) => 
                             <ListItem
                                 name={item.name}
                                 img={`${item.thumbnail.path}.${item.thumbnail.extension}`}
+                                onPress={changeToProfileScreen}
                             />
                 }
             />
@@ -48,11 +81,16 @@ const HomeScreen = ({ navigation }: { navigation: ProfileScreenNavigationProp })
 }
 
 const styles = StyleSheet.create({
-    item: {
-      padding: 10,
-      fontSize: 18,
-      height: 44,
+    textInput: {
+        height: 40,
+        margin: 12,
+        paddingHorizontal: 12,
+        borderRadius: 3,
+        backgroundColor: 'white'
     },
+    resultsCount: {
+        marginLeft: 16
+    }
   });
 
 export default HomeScreen
