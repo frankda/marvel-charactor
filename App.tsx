d36e507b3f51c3abc7a2062ecd39bd624c1d8c4f
frankda <suyunyi@gmail.com>
2021-07-25 18:45:20 +1000

    - Add react navigator types

--- App.tsx
+++ App.tsx
@@ -2,15 +2,40 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View, StatusBar, Platform } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
-const Stack = createStackNavigator();
+export type RootStackParamList = {
+  Home: undefined,
+  Profile: { name: string }
+}
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-
-      </Stack.Navigator>
+        <View style={styles.container}>
+          {Platform.OS === 'ios' && <StatusBar />}
+            <Stack.Navigator initialRouteName="Home">
+              <Stack.Screen 
+                  name="Home"
+                  component={HomeScreen}
+              />
+              <Stack.Screen 
+                  name="Profile"
+                  component={ProfileScreen}
+              />
+            </Stack.Navigator>
+        </View>
     </NavigationContainer>
   );
 }
